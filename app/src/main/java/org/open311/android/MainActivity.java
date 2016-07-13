b6ccb43c1017cdcecdac5a38401f050660648e1e
milovanderlinden <milo@dogodigi.net>
2016-07-13 22:50:30 +0200

    Cleanup mainactivity and added TLS1.2 check for Android 4.x

--- app/src/main/java/org/open311/android/MainActivity.java
+++ app/src/main/java/org/open311/android/MainActivity.java
@@ -3,20 +3,17 @@ package org.open311.android;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.SearchView;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.security.ProviderInstaller;
 
 import org.codeforamerica.open311.facade.data.ServiceRequest;
 import org.open311.android.adapters.ViewPagerAdapter;
@@ -30,14 +27,8 @@ import static org.open311.android.helpers.Utils.*;
 
 public class MainActivity extends AppCompatActivity
         implements
-        NavigationView.OnNavigationItemSelectedListener,
-        SwipeRefreshLayout.OnRefreshListener,
         RequestsFragment.OnListFragmentInteractionListener,
-        SearchView.OnQueryTextListener,
         FragmentManager.OnBackStackChangedListener {
-    private ActionBarDrawerToggle toggle;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
     private String installationId;
     private ReportFragment reportFragment;
 
@@ -52,6 +43,13 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            ProviderInstaller.installIfNeeded(this);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
         settings = getSettings(this);
         installationId = Installation.id(this);
         setContentView(R.layout.activity_main);
@@ -59,36 +57,16 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         getSupportFragmentManager().addOnBackStackChangedListener(this);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        toggle.setDrawerIndicatorEnabled(false); // Hide the hamburger icon
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        //NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        //navigationView.setNavigationItemSelectedListener(this);
-
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
         if (savedInstanceState != null) {
             // Restore the fragment's instance
             reportFragment = (ReportFragment) getSupportFragmentManager().getFragment(
                     savedInstanceState, "reportFragment");
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
         }
     }
 
@@ -136,76 +114,9 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    /**
-     * Handle navigation view item clicks here.
-     */
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-
-        Fragment fragment = null;
-        Class fragmentClass = null;
-
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.nav_reports_my:
-                fragmentClass = PolicyFragment.class;
-                break;
-            case R.id.nav_reports_recent:
-                fragmentClass = RequestsFragment.class;
-                break;
-            case R.id.nav_settings:
-                fragmentClass = ProfileFragment.class;
-                break;
-            case R.id.nav_about:
-                fragmentClass = ReportFragment.class;
-                break;
-            default: // fragmentClass = null
-        }
-
-        try {
-            if (fragmentClass != null) {
-                fragment = (Fragment) fragmentClass.newInstance();
-            }
-
-            // Insert the fragment by replacing any existing fragment
-            if (fragment != null) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).addToBackStack(null).commit();
-            }
-            if (getSupportActionBar() != null) {
-                getSupportActionBar().setTitle(item.getTitle());
-                if (toggle != null) {
-                    toggle.setDrawerIndicatorEnabled(true);
-                }
-            }
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
     @Override
     public void onListFragmentInteraction(ServiceRequest item) {
 
-    }
-
-    @Override
-    public void onRefresh() {
-
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        return false;
     }
 
     @Override
@@ -238,10 +149,10 @@ public class MainActivity extends AppCompatActivity
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), MainActivity.this);
-        adapter.addFragment(new RequestsFragment(), "Requests");
-        adapter.addFragment(new ReportFragment(), "Report");
-        adapter.addFragment(new ProfileFragment(), "Profile");
-        adapter.addFragment(new PolicyFragment(), "Policy");
+        adapter.addFragment(new ReportFragment());
+        adapter.addFragment(new RequestsFragment());
+        adapter.addFragment(new ProfileFragment());
+        adapter.addFragment(new PolicyFragment());
         viewPager.setAdapter(adapter);
     }
 }
